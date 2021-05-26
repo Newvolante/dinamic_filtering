508e3014b5a805bd9d22d4daf87b3ca3184a761c
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-05-26 17:22:46 +0200

    added forEach loop to iterate through the array and fill the table

--- script.js
+++ script.js
@@ -2,5 +2,18 @@ $(document).ready(function() {
     console.log('Script linked correctly to the page');
     console.assert(workers, 'Array of workers not found');
 
-    
+    /*creates a row for each element of the workers array and
+    appends it to the table */
+    workers.forEach(person => {
+        console.log('Iterating ' + person.name);
+
+        let $tableBody = $('tbody');
+        let $newRow = $('<tr class="row"><tr>');
+        let $name = $('<td>' + person.name + '<td>');
+        let $rate = $('<td>' + person.rate + '<td>');
+        
+        $tableBody.append($newRow);
+        $newRow.append($name)
+                    .append($rate);
+    });
 });

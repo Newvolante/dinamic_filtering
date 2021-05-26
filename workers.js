ca099f2454249577052b66ef82fd3fd437a81bce
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-05-26 16:07:19 +0200

    filled workers.js with an array of workers

---
+++ workers.js
@@ -0,0 +1,42 @@
+let workers = [
+    {
+        name: 'Bob',
+        rate: 90
+    },
+    {
+        name: 'Jim',
+        rate: 80
+    },
+    {
+        name: 'Geena',
+        rate: 10
+    },
+    {
+        name: 'Trudy',
+        rate: 70
+    },
+    {
+        name: 'Spaz',
+        rate: 50
+    },
+    {
+        name: 'Doodle',
+        rate: 150
+    },
+    {
+        name: 'Hamed',
+        rate: 30
+    },
+    {
+        name: 'Giustino',
+        rate: 160
+    },
+    {
+        name: 'Consuelo',
+        rate: 65
+    },
+    {
+        name: 'Ted',
+        rate: 20
+    },
+];
